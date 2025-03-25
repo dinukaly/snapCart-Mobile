d386bb3d83ca329f9aa4f7cf9bbe4bbe5ba269ae
Dinuka Liyanage <infodinukalk@gmail.com>
2025-03-25 10:56:54 +0530

    load products on bookmark

--- components/ui/screen/home/HomeBookmarkScreen.tsx
+++ components/ui/screen/home/HomeBookmarkScreen.tsx
@@ -1,17 +1,54 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import DisplayTypeWidget from "../shared/DisplayTypeWidget";
+import ProductGridViewWidget from "./widget/ProductGridViewWidget";
+import ProductListViewWidget from "./widget/ProductListViewWidget";
 
 export default function HomeBookmarkScreen() {
-    return(
+    const [searchQuery,setSearchQuery] = useState('');
+    const [isGridEnabled,setIsGridEnabled] = useState(true);
+    const manageGridView = ({state}:any) => {
+        setIsGridEnabled(state);
+    }
+    return (
         <View style={styles.container}>
-            <Text>Home bookmark screen</Text>
+            <Searchbar
+                placeholder="Search"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+            />
+            <DisplayTypeWidget callBack={manageGridView}/>
+            {isGridEnabled?(
+                 <ScrollView
+                 showsVerticalScrollIndicator={false}>
+                     <ProductGridViewWidget/>
+                     <ProductGridViewWidget/>
+                     <ProductGridViewWidget/>
+                     <ProductGridViewWidget/>
+                     <ProductGridViewWidget/>
+                 </ScrollView>
+            ):(
+                <ScrollView
+                showsVerticalScrollIndicator={false}>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                    <ProductListViewWidget/>
+                </ScrollView>
+            )}
+                
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 10
     }
 });
