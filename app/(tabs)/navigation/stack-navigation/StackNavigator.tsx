a913180acf97389c0a0bfead67c7ff153fc105c8
Dinuka Liyanage <infodinukalk@gmail.com>
2025-03-25 10:54:40 +0530

    some changes

--- app/(tabs)/navigation/stack-navigation/StackNavigator.tsx
+++ app/(tabs)/navigation/stack-navigation/StackNavigator.tsx
@@ -9,6 +9,13 @@ import ProfileScreen from '@/components/ui/screen/other/ProfileScreen';
 import SettingsScreen from '@/components/ui/screen/other/SettingsScreen';
 import VoucherScreen from '@/components/ui/screen/other/VoucherScreen';
 import LoginScreen from '@/components/ui/screen/security/LoginScreen';
+import MyOrdersTopTabNavigator from '../tab-navigation/MyOrdersTopTabNavigator';
+import SignUpScreen from '@/components/ui/screen/security/SignUpScreen';
+import SignUpVerifyEmailScreen from '@/components/ui/screen/security/SignUpVerifyEmailScreen';
+import ResetPasswordScreen from '@/components/ui/screen/security/ResetPasswordScreen';
+import ResetPasswordVerifyEmailScreen from '@/components/ui/screen/security/RestPasswordVerifyEmailScreen';
+import ChangePasswordScreen from '@/components/ui/screen/security/ChangePasswordScreen';
+import ProductDetailScreen from '@/components/ui/screen/ProductDetailScreen';
 const Stack = createStackNavigator();
 export default function StackNavigator() {
      return (
@@ -27,7 +34,7 @@ export default function StackNavigator() {
                     component={LatestProductScreen} />
                <Stack.Screen name={'MyOrders'}
                     options={{ title: 'My Orders here' }}
-                    component={MyOrderScreen} />
+                    component={MyOrdersTopTabNavigator} />
                <Stack.Screen name={'Notifications'}
                     options={{ title: 'Notifications here' }}
                     component={NotificationScreen} />
@@ -43,6 +50,25 @@ export default function StackNavigator() {
                <Stack.Screen name={'Login'}
                     options={{ title: 'Login here' }}
                     component={LoginScreen} />
+               <Stack.Screen name={'Signup'}
+                    options={{ title: 'Register here' }}
+                    component={SignUpScreen} />
+               <Stack.Screen name={'SignupVerifyEmail'}
+                    options={{ title: 'Verify your email' }}
+                    component={SignUpVerifyEmailScreen} />
+               <Stack.Screen name={'ResetPasswordOtp'}
+                    options={{ title: 'Reset Password Otp' }}
+                    component={ResetPasswordScreen} />
+               <Stack.Screen name={'ResetPasswordVerifyEmail'}
+                    options={{ title: 'Verify Email' }}
+                    component={ResetPasswordVerifyEmailScreen} />
+               <Stack.Screen name={'ChangePassword'}
+                    options={{ title: 'Change Password' }}
+                    component={ChangePasswordScreen} />
+                     <Stack.Screen name={'ProdcutDetails'}
+                    options={{ title: 'Product Details' }}
+                    component={ProductDetailScreen} />
           </Stack.Navigator>
+
      )
 }
